421ca673793b0a79aea806069d6f6e1577b74a7f
Erik Herczeg <erik.herczeg@gmail.com>
2020-04-17 10:28:22 +0200

    fix: Scaling map pins on HD and FHD version

--- assets/js/partials/home-map-pins-scaling.js
+++ assets/js/partials/home-map-pins-scaling.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
+$(document).ready(function () {
+  $(window).scroll(function () {
     const pins = $('.map-section .map-pins > div');
-    $(this).scrollTop() > 600
-      ? pins.css('transform', 'scale(1)')
-      : pins.css('transform', 'scale(0)');
+    if ($(window).width() <= 1366) {
+      $(this).scrollTop() >= 900
+        ? pins.css('transform', 'scale(1)')
+        : pins.css('transform', 'scale(0)');
+    } else {
+      $(this).scrollTop() >= 700
+        ? pins.css('transform', 'scale(1)')
+        : pins.css('transform', 'scale(0)');
+    }
   });
 });
