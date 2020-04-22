00de0181be753106b8a296649f6ef4c5aac8fea8
Erik Herczeg <erik.herczeg@gmail.com>
2020-04-22 13:01:26 +0200

    feat: Add 'Learn more' link to Features

--- assets/js/partials/features-show-hide-text.js
+++ assets/js/partials/features-show-hide-text.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  $(`.features .bookable-section a,
-    .features .payment-clutch-section a,
-    .features .boosted-section a,
-    .features .dashboard-section a,
-    .features .travel-section a,
-    .features .book-section a`).on('click', function(e) {
+$(document).ready(function () {
+  $(`.features .bookable-section #show-more,
+    .features .payment-clutch-section #show-more,
+    .features .boosted-section #show-more,
+    .features .dashboard-section #show-more,
+    .features .travel-section #show-more,
+    .features .book-section #show-more`).on('click', function (e) {
     e.preventDefault();
     $(this)
       .text(this.text === 'Show more' ? 'Show less' : 'Show more')
