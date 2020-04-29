47e0b218ae01152beb5b378488952596fb835cda
Erik Herczeg <erik.herczeg@gmail.com>
2020-04-29 12:38:20 +0200

    feat: Add checkbox in the contact form, jquery-ajaxchimp cdn

--- assets/js/partials/contact-form.js
+++ assets/js/partials/contact-form.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-  $('#contact-send-button').on('click', function() {
-    const emailjsConfig = $('.contact-form').data('emailjs-config');
+$(document).ready(function () {
+  $('#contact-send-button').on('click', function () {
+    const emailjsConfig = $('#contact-form').data('emailjs-config');
+    const mailchimpNewsletterFormActionUrl = $(
+      '#mc-newsletter-form-action-url',
+    ).data('mc-newsletter-form-action-url');
 
     const email = $('#email');
-    const firstName = $('#first-name');
-    const lastName = $('#last-name');
+    const firstName = $('#firstName');
+    const lastName = $('#lastName');
     const message = $('#message');
+    const isNewsletter = $('#newsletter').prop('checked');
 
     const isValidEmail = validate('email', email);
     const isValidFirstName = validate('firstName', firstName);
@@ -18,11 +22,20 @@ $(document).ready(function() {
         .css('opacity', '.7')
         .attr('disabled', 'disabled');
 
+      if (isNewsletter) {
+        $('#contact-form').ajaxChimp({
+          url: mailchimpNewsletterFormActionUrl,
+          callback: isFailedNewsletterSubscription,
+        });
+        $(this).submit();
+      }
+
       const templateParams = {
         email: email.val(),
         firstname: firstName.val(),
         lastname: lastName.val(),
         message: message.val(),
+        newsletter: isNewsletter ? 'Yes' : 'No',
       };
 
       emailjs
@@ -33,10 +46,10 @@ $(document).ready(function() {
           emailjsConfig.emailjs_user_id,
         )
         .then(
-          function(response) {
+          function (response) {
             isSendEmail('success');
           },
-          function(err) {
+          function (err) {
             isSendEmail('error');
           },
         );
@@ -58,30 +71,33 @@ function validate(fieldName, element) {
       return false;
     } else {
       element.removeClass('is-invalid');
-      element
-        .siblings('.alert')
-        .css('display', 'none')
-        .text('');
+      element.siblings('.alert').css('display', 'none').text('');
       return true;
     }
   } else {
     element.addClass('is-invalid');
-    element
-      .siblings('.alert')
-      .css('display', 'block')
-      .text('Please fill in');
+    element.siblings('.alert').css('display', 'block').text('Please fill in');
+    return false;
+  }
+}
+
+function isFailedNewsletterSubscription(res) {
+  if (res.result === 'success') {
+    $('.contact-success-box h3').css('margin-bottom', '16px');
+    $('.contact-success-box h4').css('display', 'block');
+    return true;
+  } else {
+    console.error('FAILURE: ', res.msg);
     return false;
   }
 }
 
 function isSendEmail(status) {
-  $('.contact-form')
-    .trigger('reset')
-    .css('display', 'none');
+  $('.contact-form').trigger('reset').css('display', 'none');
   $('.spinner-border').removeAttr('style');
   $('#contact-send-button').removeAttr('style disabled');
   $('.contact-' + status + '-box').css('display', 'block');
-  $('#contact-' + status + '-button').on('click', function() {
+  $('#contact-' + status + '-button').on('click', function () {
     $('.contact-' + status + '-box').css('display', 'none');
     $('.contact-form').css('display', 'block');
   });
