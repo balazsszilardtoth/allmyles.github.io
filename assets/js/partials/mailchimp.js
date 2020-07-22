44fc0fe0ef5ded47f94bbd4cfe136ae4cecf7415
Erik Herczeg <erik.herczeg@gmail.com>
2020-07-22 14:45:54 +0200

    feat: Add to listen for the DOMContentLoaded with scripts

--- assets/js/partials/mailchimp.js
+++ assets/js/partials/mailchimp.js
@@ -1,7 +1,9 @@
-require(['mojo/signup-forms/Loader'], function (L) {
-  L.start({
-    baseUrl: 'mc.us8.list-manage.com',
-    uuid: '34427879ce889cb69ae1b9e75',
-    lid: '7e58348ec9',
+$(document).ready(function () {
+  require(['mojo/signup-forms/Loader'], function (L) {
+    L.start({
+      baseUrl: 'mc.us8.list-manage.com',
+      uuid: '34427879ce889cb69ae1b9e75',
+      lid: '7e58348ec9',
+    });
   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -45,8 +45,7 @@ gulp.task('vendor-js', function () {
   return gulp
     .src([
       'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/popper.js/dist/umd/popper.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
       'node_modules/lazysizes/lazysizes.min.js',
       'node_modules/cookieconsent/build/cookieconsent.min.js',
       'node_modules/emailjs-com/dist/email.min.js',
