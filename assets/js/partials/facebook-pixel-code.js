bb1c34c6991290fb9de725614797cd10881f1f3b
Erik Herczeg <erik.herczeg@gmail.com>
2020-07-23 13:32:00 +0200

    chore: Move FB sdk external link to local file, rewrite code of fb js file

--- assets/js/partials/facebook-pixel-code.js
+++ assets/js/partials/facebook-pixel-code.js
@@ -1,23 +1,19 @@
-!(function (f, b, e, v, n, t, s) {
-  if (f.fbq) return;
-  n = f.fbq = function () {
-    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-  };
-  if (!f._fbq) f._fbq = n;
-  n.push = n;
-  n.loaded = !0;
-  n.version = '2.0';
-  n.queue = [];
-  t = b.createElement(e);
-  t.async = !0;
-  t.src = v;
-  s = b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t, s);
-})(
-  window,
-  document,
-  'script',
-  'https://connect.facebook.net/en_US/fbevents.js',
-);
-fbq('init', '877131375794306');
-fbq('track', 'PageView');
+$(document).ready(function () {
+  !(function (f, b, e, v, n, t) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    b.body.appendChild(t);
+  })(window, document, 'script', '/assets/js/facebook-events.min.js');
+  fbq('init', '877131375794306');
+  fbq('track', 'PageView');
+});
