d553e4fb344a329d388fa0198b85f92b11797986
Erik Herczeg <erik.herczeg@gmail.com>
2020-07-23 13:19:45 +0200

    fix: Rewrite code of tawk.io and gtm

--- assets/js/partials/google-tag-manager.js
+++ assets/js/partials/google-tag-manager.js
@@ -1,10 +1,11 @@
-(function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-WMV9FM');
+$(document).ready(function () {
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = !0;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    d.body.appendChild(j);
+  })(window, document, 'script', 'dataLayer', 'GTM-WMV9FM');
+});
--- assets/js/partials/tawk-to.js
+++ assets/js/partials/tawk-to.js
@@ -1,11 +1,11 @@
-var Tawk_API = Tawk_API || {},
-  Tawk_LoadStart = new Date();
-(function () {
-  var s1 = document.createElement('script'),
-    s0 = document.getElementsByTagName('script')[0];
-  s1.async = true;
-  s1.src = 'https://embed.tawk.to/59d349974854b82732ff33e4/default';
-  s1.charset = 'UTF-8';
-  s1.setAttribute('crossorigin', '*');
-  s0.parentNode.insertBefore(s1, s0);
-})();
+$(document).ready(function () {
+  var Tawk_API = Tawk_API || {};
+  (function () {
+    var s = document.createElement('script');
+    s.async = !0;
+    s.src = 'https://embed.tawk.to/59d349974854b82732ff33e4/default';
+    s.charset = 'UTF-8';
+    s.setAttribute('crossorigin', '*');
+    document.body.appendChild(s);
+  })();
+});
