1eae0c29ea9aec12096b170d74049af6ec41000c
Erik Herczeg <erik.herczeg@gmail.com>
2020-07-24 08:49:00 +0200

    fix: Update tawk-to.js

--- assets/js/partials/tawk-to.js
+++ assets/js/partials/tawk-to.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-  var Tawk_API = Tawk_API || {};
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
   (function () {
     var s = document.createElement('script');
-    s.async = !0;
+    s.async = 1;
     s.src = 'https://embed.tawk.to/59d349974854b82732ff33e4/default';
     s.charset = 'UTF-8';
     s.setAttribute('crossorigin', '*');
