9438d79e723b5eca4c9f3c052892940cda412ebf
Erik Herczeg <erik.herczeg@gmail.com>
2020-08-04 10:10:07 +0200

    chore: Add hotjar js

---
+++ assets/js/partials/hotjar.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  (function (h, o, t, j, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+    h._hjSettings = { hjid: 1462355, hjsv: 6 };
+    r = o.createElement('script');
+    r.async = 1;
+    r.defer = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    o.body.appendChild(r);
+  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+});
