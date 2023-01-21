9868b43978c991520c612ecdcb3419c2305bd6ce
Balazs Toth <balazs.szilard.toth@icloud.com>
2023-01-21 15:32:34 +0100

    create turstile.js

---
+++ assets/js/partials/turstile.js
@@ -0,0 +1,2 @@
+window.onloadTurnstileCallback = function () {    turnstile.render('#myWidget', {        sitekey: '0x4AAAAAAACFi4Df3aq41Qm3',        callback: function(token) {            console.log(`Challenge Success ${token}`);        },    });
+};
