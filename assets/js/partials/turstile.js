5e5f0fe245799d1c2def9d72a3f2f1401633df2b
Balazs Toth <balazs.szilard.toth@icloud.com>
2023-01-21 16:02:28 +0100

    delete turstile.js for tipo reasons

--- assets/js/partials/turstile.js
+++
@@ -1,8 +0,0 @@
-window.onloadTurnstileCallback = function() {
-	turnstile.render('#myWidget', {
-		sitekey: '0x4AAAAAAACFi4Df3aq41Qm3',
-		callback: function(token) {
-			console.log(`Challenge Success ${token}`);
-		},
-	});
-};
