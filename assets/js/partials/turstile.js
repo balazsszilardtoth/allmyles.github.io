28bd8a5c6988f848c35968afb1351582c6b8fa73
Balazs Toth <balazs.szilard.toth@icloud.com>
2023-01-21 15:41:41 +0100

    fix indentation

--- assets/js/partials/turstile.js
+++ assets/js/partials/turstile.js
@@ -1,2 +1,8 @@
-window.onloadTurnstileCallback = function () {    turnstile.render('#myWidget', {        sitekey: '0x4AAAAAAACFi4Df3aq41Qm3',        callback: function(token) {            console.log(`Challenge Success ${token}`);        },    });
+window.onloadTurnstileCallback = function() {
+	turnstile.render('#myWidget', {
+		sitekey: '0x4AAAAAAACFi4Df3aq41Qm3',
+		callback: function(token) {
+			console.log(`Challenge Success ${token}`);
+		},
+	});
 };
